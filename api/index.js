694ce3245c2573acb8905eaab00ad6b5acc42c1b
HUZAIM-BIN-ASAD <huzaimbinasad@gmail.com>
2025-04-18 19:08:26 +0500

    :S:

--- api/index.js
+++ api/index.js
@@ -11,7 +11,11 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'YOUR_VERCEL_APP_URL'],
+  origin: [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+    'https://echho.vercel.app'
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
