40929acb1201ca329c9fa6fbcaefc7e000427ca0
HUZAIM-BIN-ASAD <huzaimbinasad@gmail.com>
2025-04-30 14:36:09 +0500

    Updated UserQuerry with fullName

--- api/userInfo.js
+++ api/userInfo.js
@@ -59,8 +59,7 @@ router.get('/userinfo', async (req, res) => {
     const userData = {
       user: {
         user_id: user.user_id,
-        first_name: user.first_name,
-        last_name: user.last_name,
+        full_name: user.full_name,
         email: user.email,
         username: user.username,
         gender: user.gender,
